bbbf62dbe579e4d7ccf55acd50d19344ae989885
Divya Koyyalagunta <divyakoyyalagunta@Divyas-MacBook-Pro.local>
2017-11-02 00:37:43 -0400

    updated training

--- code/good.m
+++ code/good.m
@@ -1,20 +1,20 @@
 function [layers, options] = good(checkpointPath)
 
-layers = [imageInputLayer([1 2]) %  inputs images to a network and applies data normalization
-    fullyConnectedLayer(12) % multiplies the input by a weight matrix and then adds a bias vector., 2 = size of output for the fully connected layer
-    reluLayer % creates a ReLU layer
-    fullyConnectedLayer(12) % multiplies the input by a weight matrix and then adds a bias vector., 2 = size of output for the fully connected layer
-    reluLayer % creates a ReLU layer
+layers = [imageInputLayer([1 2]) 
+    fullyConnectedLayer(12) 
+    reluLayer
+    fullyConnectedLayer(10) 
+    reluLayer 
     fullyConnectedLayer(2)
-    softmaxLayer % makes input to classification layer behave like probabilities (all positive and add up to 1)
+    softmaxLayer 
     classificationLayer()];
 
 % Learning rate schedule, based on the following relation:
 % finalRate = initialRate * factor ^ (epochs/period)
-miniBatchSize = 20;
+miniBatchSize = 15;
 initialRate = 0.1;
-finalRate = initialRate;
-epochs = 100;
+finalRate = initialRate/10;
+epochs = 130;
 dropFactor = .8;
 if initialRate <= finalRate
     dropPeriod = epochs;
